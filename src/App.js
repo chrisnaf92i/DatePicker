01a76018ee30663c86119bf7a64200fb2e9a3891
christopher nafrere <nafrere.christopher@gmail.com>
2022-07-08 17:29:26 +0200

    final result

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const today = new Date;
+  const today = new Date();
   const months = [
     "January",
     "February",
@@ -19,40 +19,72 @@ function App() {
   ];
 
   const [date, setDate] = useState("");
-  const [daySelected, setDaySelected] = useState("");
-  const [monthSelected, setMonthSelected] = useState("");
-  const [yearSelected, setYearSelected] = useState("");
-  const [timeSelected, setTimeSelected] = useState("");
+  const [selectedDay, setSelectedDay] = useState("");
+  const [selectedMonth, setSelectedMonth] = useState("");
+  const [selectedYear, setSelectedYear] = useState("");
+  const [selectedTime, setSelectedTime] = useState("");
 
 
   useEffect(() => {
-    setDate(`${today.getDay()}/${today.getMonth()}/${today.getFullYear()} | ${today.getHours()}:${today.getMinutes()}`);
-    setDaySelected(today.getDay());
-    setMonthSelected(today.getMonth());
-    setYearSelected(today.getFullYear());
+    setSelectedDay(today.getDay());
+    setSelectedMonth(today.getMonth());
+    setSelectedYear(today.getFullYear());
+    setSelectedTime("00:00 AM")
+    setDate(`${selectedDay}/${selectedMonth}/${selectedYear} | ${selectedTime} `);
+
   }, [])
 
-  const reset = () => {
-    setDate(`${today.getDay()}/${today.getMonth()}/${today.getFullYear()} | ${today.getHours()}:${today.getMinutes()}`);
+  const resetSelectedDate = () => {
+    setSelectedDay(today.getDay());
+    setSelectedMonth(today.getMonth());
+    setSelectedYear(today.getFullYear());
+    setDate(`${selectedDay}/${selectedMonth}/${selectedYear}`);
+  }
+  const handleClickGoToNextMonth = () => {
+    
+      setSelectedMonth(selectedMonth + 1);
+    
+    if(selectedMonth >= 11){
+      setSelectedMonth(0);
+      setSelectedYear(selectedYear + 1)
+
+    }
+
+    setDate(`${selectedDay}/${selectedMonth + 1}/${selectedYear}`);
+
+  }
+  const handleClickGoToPreviousMonth = () => {
+    
+    setSelectedMonth(selectedMonth - 1);
+  
+  if(selectedMonth <= 0){
+    setSelectedMonth(11);
+    setSelectedYear(selectedYear - 1);
   }
 
+  setDate(`${selectedDay}/${selectedMonth + 1}/${selectedYear}`);
+
+  
+}
+
+  
   return (
     <main className="App">
-      <h1> Schedule Response </h1>
+      <h1 className='Title'> Schedule Response </h1>
 
-      <header>
-        <p> Date <span> {daySelected}/{monthSelected}/{yearSelected} </span> </p>
-        <p> Time <span> HH/MM </span> </p>
+      <header className="HeaderResult">
+        <p className="Result"> Date <span className="ResultData"> {selectedDay}/{selectedMonth + 1}/{selectedYear} </span> </p>
+        <p className="Result"> Time <span className="ResultData"> HH/MM </span> </p>
       </header>
 
       <section>
-        <article>
-          <div>
-            <p> {months[monthSelected]} {yearSelected} </p>
+        <article className='Calendar'>
+          <div className="MonthSelector">
+            <p className='MonthSelected'> {months[selectedMonth]} {selectedYear} </p>
 
             <p>
-              <button>Back</button>
-              <button>Next</button>
+              <button className='NavMonth' onClick={handleClickGoToPreviousMonth}> {"<"} </button>
+              <button className='NavMonth' onClick={handleClickGoToNextMonth}>{">"}</button>
             </p>
           </div>
 
@@ -64,18 +96,17 @@ function App() {
           <div>
             <p> Time <span> Heure selectionner </span> </p>
           </div>
-          
-          
-          
+
+
 
         </aside>
 
       </section>
 
-      <button onClick={() => {
+      <button className='btn--validate' onClick={() => {
         console.log(date);
       }}> Schedule </button>
-      <button onClick={reset}> Cancel </button>
+      <button className='btn--cancel' onClick={resetSelectedDate}> Cancel </button>
     </main>
   );
 }
