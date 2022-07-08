abaf7d2f8ee6329fa36eb1c6432be2ce7a6ab64a
christopher nafrere <nafrere.christopher@gmail.com>
2022-07-08 15:50:55 +0200

    Création d'une version sans selecteur de date, d'heure et sans CSS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const today = new Date;
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+
+  const [date, setDate] = useState("");
+  const [daySelected, setDaySelected] = useState("");
+  const [monthSelected, setMonthSelected] = useState("");
+  const [yearSelected, setYearSelected] = useState("");
+  const [timeSelected, setTimeSelected] = useState("");
+
+
+  useEffect(() => {
+    setDate(`${today.getDay()}/${today.getMonth()}/${today.getFullYear()} | ${today.getHours()}:${today.getMinutes()}`);
+    setDaySelected(today.getDay());
+    setMonthSelected(today.getMonth());
+    setYearSelected(today.getFullYear());
+  }, [])
+
+  const reset = () => {
+    setDate(`${today.getDay()}/${today.getMonth()}/${today.getFullYear()} | ${today.getHours()}:${today.getMinutes()}`);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main className="App">
+      <h1> Schedule Response </h1>
+
+      <header>
+        <p> Date <span> {daySelected}/{monthSelected}/{yearSelected} </span> </p>
+        <p> Time <span> HH/MM </span> </p>
       </header>
-    </div>
+
+      <section>
+        <article>
+          <div>
+            <p> {months[monthSelected]} {yearSelected} </p>
+
+            <p>
+              <button>Back</button>
+              <button>Next</button>
+            </p>
+          </div>
+
+          
+        </article>
+        
+
+        <aside>
+          <div>
+            <p> Time <span> Heure selectionner </span> </p>
+          </div>
+          
+          
+          
+
+        </aside>
+
+      </section>
+
+      <button onClick={() => {
+        console.log(date);
+      }}> Schedule </button>
+      <button onClick={reset}> Cancel </button>
+    </main>
   );
 }
 
